1d00e3885a12a404aea437009a7c5b74ac38ad45
Elizabeth Doyle <123044077+elizabethmdoyle@users.noreply.github.com>
2024-02-26 12:18:23 -0500

    5 passing tests

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,72 @@
-const checkCarId = (req, res, next) => {
+const Car = require('./cars-model');
+const db = require('../../data/db-config');
+var vinValidator = require('vin-validator');
+
+exports.checkCarId = async (req, res, next) => {
   // DO YOUR MAGIC
+  //checkCarId returns a status 404 with a { message: "car with id <car id> is not found" } if the id in req.params does not exist in the database.
+
+  try {
+    const car = await Car.getById(req.params.id);
+
+    if(!car) {
+      next({status: 404, message: "car with id <car id> is not found" })
+    } else {
+      req.car = car
+      next()
+    }
+  } catch(err) {
+    next(err)
+  }
 }
 
-const checkCarPayload = (req, res, next) => {
+exports.checkCarPayload = (req, res, next) => {
   // DO YOUR MAGIC
+
+  //returns a status 400 with a { message: "<field name> is missing" } if any required field is missing.
+
+  const error = {status:400}
+  const {vin, make, model, mileage, title, transmission } = req.body;
+
+
+
 }
 
-const checkVinNumberValid = (req, res, next) => {
+exports.checkVinNumberValid = async (req, res, next) => {
   // DO YOUR MAGIC
+  //checkVinNumberValid returns a status 400 with a { message: "vin <vin number> is invalid" } if the vin number is invalid.
+
+  // var isValidVin = vinValidator.validate('11111111111111111'); // true
+
+  try {
+    const validVin = await('cars').where('vin', req.body.vin.trim()).first()
+
+    if(validVin == vinValidator.validate(validVin)) {
+      next({ status: 400, message:"vin <vin number> is invalid" })
+    } else {
+      next()
+    }
+  } catch (err) {
+    next(err)
+  }
 }
 
-const checkVinNumberUnique = (req, res, next) => {
+exports.checkVinNumberUnique = async (req, res, next) => {
   // DO YOUR MAGIC
+
+  //checkVinNumberUnique returns a status 400 with a { message: "vin <vin number> already exists" } if the vin number already exists in the database.
+
+  try {
+    const number = await('cars').where('vin', req.body.vin.trim()).first()
+
+    if(number) {
+      next({ status: 400, message:"vin <vin number> already exists" })
+    } else {
+      next()
+    }
+  } catch (err) {
+    next(err)
+  }
 }
+
+
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -2,37 +2,44 @@
 const express = require('express');
 const router = express.Router();
 const Car = require('./cars-model');
-const { checkCarId, , checkCarPayload, checkVinNumberValid, checkVinNumberUnique } = require('./cars-middleware');
+const { checkCarId, checkCarPayload, checkVinNumberValid, checkVinNumberUnique } = require('./cars-middleware');
 
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
     try {
-
+        const cars = await Car.getAll();
+        res.json(cars)
     } catch (err) {
         next(err)
     }
 })
 
-router.get('/:id', (req, res, next) => {
-    try {
+router.get('/:id', checkCarId, async (req, res, next) => {
+   res.json(req.car)
+})
 
+router.post('/', checkCarPayload, checkVinNumberUnique, checkVinNumberValid, async (req, res, next) => {
+    try {
+        const newCar = await Car.create({
+               vin: req.body.vin.trim(), 
+                make: req.body.make.trim(), 
+                model: req.body.model.trim(), 
+                mileage: req.body.mileage.trim(), 
+                title: req.body.title.trim(), 
+                transmission: req.body.make.trim()
+              
+        })
+        res.status(201).json(newCar)
     } catch (err) {
         next(err)
     }
 })
 
-router.post('/', (req, res, next) => {
-    try {
-
-    } catch (err) {
-        next(err)
-    }
-})
-
-router.use(err, req, res, next) => {
-    res.status(err.status || 500).JSON({
-        message: err.message
+router.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message,
+        stack: err.stack
     })
-}
+})
 
 module.exports = router;
