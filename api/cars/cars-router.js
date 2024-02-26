1d64a15055c0e8248edf128c298a343430ee7fcc
Elizabeth Doyle <123044077+elizabethmdoyle@users.noreply.github.com>
2024-02-26 11:41:16 -0500

    working on router

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,38 @@
 // DO YOUR MAGIC
+const express = require('express');
+const router = express.Router();
+const Car = require('./cars-model');
+const { checkCarId, , checkCarPayload, checkVinNumberValid, checkVinNumberUnique } = require('./cars-middleware');
+
+
+router.get('/', (req, res, next) => {
+    try {
+
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.get('/:id', (req, res, next) => {
+    try {
+
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.post('/', (req, res, next) => {
+    try {
+
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.use(err, req, res, next) => {
+    res.status(err.status || 500).JSON({
+        message: err.message
+    })
+}
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -3,5 +3,17 @@ const express = require("express")
 const server = express()
 
 // DO YOUR MAGIC
+const carsRouter = require('./cars/cars-router');
+
+server.use(express.json());
+
+
+server.use('/api/cars', carsRouter);
+
+server.use('*', (req, res)=> {
+    res.status(404).json({
+        message: 'endpoint not found'
+    })
+})
 
 module.exports = server
